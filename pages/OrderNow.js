6f336529af6a0fd8464e9ee5cd5ceb90de2ffa1c
Joe Fisher <josephfish1973@gmail.com>
2023-04-22 16:39:11 -0400

    debuggin

--- pages/OrderNow.js
+++ pages/OrderNow.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ProductList from "../components/shopping/ProductList";
-import CategoryMenu from "../components/shopping/CategoryMenu";
-import ChooseARescue from "./shopping/ChooseRescue";
+// import ProductList from "../components/shopping/ProductList";
+// import CategoryMenu from "../components/shopping/CategoryMenu";
+// import ChooseARescue from "./shopping/ChooseRescue";
 
 const OrderNow = () => {
   return (
     <div>
       <div className="w-full">
-        <ChooseARescue />
+        {/* <ChooseARescue />
         <CategoryMenu />
-        <ProductList />
+        <ProductList /> */}
 
       </div>
     </div>
