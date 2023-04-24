b307f345a1b37c716f02923cca8c712b847b6dda
Joe Fisher <josephfish1973@gmail.com>
2023-04-23 20:36:01 -0400

    find a rescue now populating

--- config/connections.js
+++ config/connections.js
@@ -2,6 +2,7 @@ import * as mongoose from "mongoose";
 
 
 const chowDb = mongoose.createConnection (
+    "mongodb+srv://joefish1973:!Fish6305@rescue-chow.qwstvcn.mongodb.net/rescue-chow",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
