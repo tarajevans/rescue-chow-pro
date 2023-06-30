bf3d84b1dd9bd47fd7aff9a1cfee32b10e3c0c57
Joe Fisher <josephfish1973@gmail.com>
2023-06-30 12:40:19 -0400

    yup

--- pages/api/stripe/webhook.js
+++ pages/api/stripe/webhook.js
@@ -8,7 +8,7 @@ export const config = {
     },
 };
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { 
     apiVersion: "2020-08-27",
 });
 
